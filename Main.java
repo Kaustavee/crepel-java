3c26f8305ceb5404465b9cae755bf5e2f22a140c
Akash Lohar <94453657+akash1047@users.noreply.github.com>
2023-08-17 20:32:25 +0530

    imported Scanner class

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
-public class Main
-{
-    public static void main(String[] args){ 
-    Scanner scanner = new Scanner(System.in);
-     while (true) { 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
             System.out.println("Enter something: ");
             String userInput = scanner.nextLine();
-            
+
             System.out.println("You entered: " + userInput);
         }
     }
