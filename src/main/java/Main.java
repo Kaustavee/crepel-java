f877e79047fcb96626250cfd6a0dfe57cc01e7a5
bobby <bobbypoddar2018@gmail.com>
2023-08-17 23:03:01 +0530

    gradle build system integrated

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter something: ");
+            String userInput = scanner.nextLine();
+
+            System.out.println("You entered: " + userInput);
+        }
+    }
+}
