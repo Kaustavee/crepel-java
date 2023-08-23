99cfc680317ce0fc906d3c7f094764359ae779f1
Akash Lohar <94453657+akash1047@users.noreply.github.com>
2023-08-23 21:07:37 +0530

    lexing arithmetic operators

---
+++ src/test/java/lexer/LexerStateTest.java
@@ -0,0 +1,12 @@
+package lexer;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LexerStateTest {
+
+    @Test
+    void checkStateONreadChar() {
+        assertTrue(true);
+    }
+}
